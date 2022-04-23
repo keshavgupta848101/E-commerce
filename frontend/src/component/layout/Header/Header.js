37a8c229a07b9f70f3b33360b08434241d1113ac
KESHAV GUPTA <iamkeshavgupta27@gmail.com>
2022-04-23 21:25:09 +0530

    Ecommerce

---
+++ frontend/src/component/layout/Header/Header.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { ReactNavbar } from "overlay-navbar";
+import logos from "../../../images/Mainlogo.png";
+import { BiSearchAlt2, BiCartAlt, BiUserPin } from "react-icons/bi";
+
+// import { GrUser } from "react-icons/gr";
+
+const options = {
+  burgerColor: "red",
+  burgerColorHover: "#eb4034",
+  logos,
+  searchIcon: true,
+  logoWidth: "20vmax",
+  navColor1: "white",
+  logoHoverSize: "10px",
+  logoHoverColor: "#eb4034",
+  link1Text: "Home",
+  link2Text: "Products",
+  link3Text: "Contact",
+  link4Text: "About",
+  link1Url: "/",
+  link2Url: "/products",
+  link3Url: "/contact",
+  link4Url: "/about",
+  link1Size: "1.3vmax",
+  link1Color: "rgba(35, 35, 35,0.8)",
+  nav1justifyContent: "flex-end",
+  nav2justifyContent: "flex-end",
+  nav3justifyContent: "flex-start",
+  nav4justifyContent: "flex-start",
+  link1ColorHover: "#eb4034",
+  link1Margin: "1vmax",
+  profileIconUrl: "/login",
+  profileIconColor: "rgba(35, 35, 35,0.8)",
+  searchIconColor: "rgba(35, 35, 35,0.8)",
+  cartIconColor: "rgba(35, 35, 35,0.8)",
+  profileIconColorHover: "#eb4034",
+  searchIconColorHover: "#eb4034",
+  cartIconColorHover: "#eb4034",
+  cartIconMargin: "1vmax",
+};
+
+const Header = () => {
+  return (
+    <ReactNavbar
+      // nav1FlexDirection="column"
+      // nav2FlexDirection="row"
+      // nav3FlexDirection="column"
+      // nav4FlexDirection="column"
+      burgerColor="white"
+      burgerColorHover="black"
+      logo={logos}
+      logoWidth="20vmax"
+      navColor1="white"
+      logoHoverSize="10px"
+      logoHoverColor="#eb4034"
+      link1Text="Home"
+      link2Text="Products"
+      link3Text="Contact"
+      link4Text="About"
+      link1Url="/"
+      link2Url="/products"
+      link3Url="/contact"
+      link4Url="/about"
+      link1Size="1.3vmax"
+      link1Color="rgba(35, 35, 35,0.8)"
+      nav1justifyContent="flex-end"
+      nav2justifyContent="flex-end"
+      nav3justifyContent="flex-start"
+      nav4justifyContent="flex-start"
+      link1ColorHover="#eb4034"
+      link1Margin="1vmax"
+      profileIconUrl="/login"
+      profileIconColor="rgba(35, 35, 35,0.8)"
+      searchIconColor="rgba(35, 35, 35,0.8)"
+      cartIconColor="rgba(35, 35, 35,0.8)"
+      profileIconColorHover="#eb4034"
+      searchIconColorHover="#eb4034"
+      cartIconColorHover="#eb4034"
+      cartIconMargin="1vmax"
+      searchIcon={true}
+      SearchIconElement={BiSearchAlt2}
+      cartIcon={true}
+      CartIconElement={BiCartAlt}
+      profileIcon={true}
+      ProfileIconElement={BiUserPin}
+    />
+  );
+};
+
+export default Header;
